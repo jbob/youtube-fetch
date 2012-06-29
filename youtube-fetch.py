db11e78e2985d82995c68daf9709eb8603dafa03
Markus Koch <koch_markus@gmx.de>
2012-06-29 22:46:08 +0200

    Initial commit

---
+++ youtube-fetch.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import subprocess
+import feedparser
+import datetime
+
+USER = sys.argv[1]
+URL = 'http://gdata.youtube.com/feeds/base/users/'+USER+'/newsubscriptionvideos'
+DOWNLOADDIR = '/home/markus/platte1000/downloads/youtube/'
+
+class Downloaded:
+  def __init__(self, dbfile=DOWNLOADDIR+'.dbfile.txt'):
+    self.dbfile = dbfile
+    db = file
+    try:
+      db = open(self.dbfile)
+    except IOError:
+      db = open(self.dbfile, 'w')
+      db.close()
+      db = open(self.dbfile)
+
+    self.downloaded = []
+    for line in db:
+      self.downloaded.append(line.strip())
+    db.close()
+
+  def get(self, link):
+    if link in self.downloaded:
+      return True
+    else:
+      return False
+
+  def add(self, link):
+    self.downloaded.append(link)
+    db = open(self.dbfile, 'w')
+    db.write('\n'.join(self.downloaded))
+    db.close()
+
+def main():
+  downloaded = Downloaded()
+  # First delte old files
+  files = os.listdir(DOWNLOADDIR)
+  for f in files:
+    modified = datetime.datetime.fromtimestamp(os.path.getmtime(DOWNLOADDIR+f))
+    if (datetime.datetime.now() - modified) > datetime.timedelta(days=14):
+      os.remove(DOWNLOADDIR+f)
+
+  # Get current feed and download new videos
+  feed = feedparser.parse(URL)
+  for entry in feed.entries:
+    link = entry.link
+    if not downloaded.get(link):
+      print "Downloading:", link
+      result = subprocess.call(['youtube-dl', '-c', '-o', DOWNLOADDIR+'%(upload_date)s_%(stitle)s_%(uploader)s.avi', link])
+      if result == 0:
+        print "Download successfull"
+        downloaded.add(link)
+      else:
+        print "Fehler"
+        exit(1)
+    else:
+      print "Skipping:", link
+
+
+if __name__ == '__main__':
+  main()
